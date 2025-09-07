13123e81b1a6b516442c6b9918275d3e4f481a70
kanoloa <kanoloa@gmail.com>
2025-09-07 21:16:43 +0900

    2nd commit

--- main.ts
+++ main.ts
@@ -1,51 +1,40 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
 
-// CodebeamerServer interface holds all the necessary information to access Codebeamer REST API.
-interface CodebeamerServer {
-  username: string | undefined;
-  password: string | undefined;
-  serverUrl: string | undefined;
+// import {ProjectReference} from "./types/codebeamer";
+
+export interface CodebeamerServer {
+  username: string | undefined,
+  password: string | undefined,
+  serverUrl: string | undefined,
   proxyUrl: string | undefined
 }
 
-const server: CodebeamerServer = {
-  username: '',
-  password: '',
-  serverUrl: '',
-  proxyUrl: ''
+function setHeaders(cb: CodebeamerServer) {
+  const headers = new Headers();
+  if (cb.username && cb.password) {
+    headers.append("Accept", "application/json");
+    headers.append("Content-Type", "application/json");
+    headers.append('Authorization', 'Basic ' + btoa(`${cb.username}:${cb.password}`));
+    console.log("setHeaders: Authorization: " + headers.get("Authorization"));
+  } else {
+    console.log("setHeaders: immature server information.");
+  }
+  return headers;
 }
 
-// Check if mandatory variables are passed from environments.
-//
-if (! Deno.env.get("USERNAME")) {
-  console.log("USERNAME is missing in your environment variables.")
+async function doFetch(target: string, cb: CodebeamerServer) {
+  const headers: Headers = setHeaders(cb);
+  return await fetch(target, {headers: headers})
+      .then((response: Response) => {
+        return response.json();
+      })
+      .then((jsonData) => {
+        return jsonData;
+      })
 }
 
-if (! Deno.env.get("PASSWORD")) {
-  console.log("PASSWORD is missing in your environment variables.");
-  Deno.exit(1);
+export function getProjects(cb: CodebeamerServer) {
+  const target = cb.serverUrl + "/projects";
+  return doFetch(target, cb);
 }
-
-if (! Deno.env.get("SERVER_URL")) {
-  console.log("SERVER_URL is missing in your environment variables.");
-  Deno.exit(1);
-}
-
-server.username = Deno.env.get("USERNAME");
-server.password = Deno.env.get("PASSWORD");
-server.serverUrl = Deno.env.get("SERVER_URL");
-
-if (Deno.env.get("PROXY_URL")) {
-  server.proxyUrl = Deno.env.get("PROXY_URL");
-}
-
-console.log("\n");
-console.log("=== E N V I R O N M E N T S ===");
-console.log("Username  : " + server.username);
-console.log("Password  : " + server.password);
-console.log("Server    : " + server.serverUrl);
-console.log("Proxy     : " + server.proxyUrl);
 
 
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,27 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertNotEquals } from "@std/assert";
+import { CodebeamerServer, getProjects } from "./main.ts";
+import {ProjectReference} from "./types/codebeamer.d.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
-});
+
+Deno.test(async function getProjectTest() {
+  const cb: CodebeamerServer = {} as CodebeamerServer;
+  cb.username = Deno.env.get("USERNAME");
+  cb.password = Deno.env.get("PASSWORD");
+  cb.serverUrl = Deno.env.get("SERVER_URL");
+  console.log("test: from env: username: " + cb.username);
+  console.log("test: from env: password: " + cb.password);
+  console.log("test: from env: server  : " + cb.serverUrl);
+  console.log("test: from env: proxy   : " + Deno.env.get("HTTPS_PROXY"));
+
+  const res: Array<ProjectReference> = await getProjects(cb);
+  // console.log(JSON.stringify(res, null, 2));
+  console.log("test: result has " + res.length + " projects.");
+  assertNotEquals(res.length, 0);
+
+  // deno-lint-ignore no-explicit-any
+  res.forEach((project): any => {
+    console.log("id = " + project.id + ", name = " + project.name);
+  })
+})
+
+
