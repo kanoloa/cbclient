051027819b4886deb05360a359a91ee862c77899
kanoloa <kanoloa@gmail.com>
2025-09-07 22:04:09 +0900

    2nd commit

--- main.ts
+++ main.ts
@@ -1,27 +1,29 @@
 
 // import {ProjectReference} from "./types/codebeamer";
 
-export interface CodebeamerServer {
+import {ProjectReference, TrackerItemReferenceSearchResult} from "./types/codebeamer.d.ts";
+
+export interface cbinit {
   username: string | undefined,
   password: string | undefined,
   serverUrl: string | undefined,
   proxyUrl: string | undefined
 }
 
-function setHeaders(cb: CodebeamerServer) {
+function setHeaders(cb: cbinit) {
   const headers = new Headers();
   if (cb.username && cb.password) {
     headers.append("Accept", "application/json");
     headers.append("Content-Type", "application/json");
     headers.append('Authorization', 'Basic ' + btoa(`${cb.username}:${cb.password}`));
-    console.log("setHeaders: Authorization: " + headers.get("Authorization"));
+    // console.log("setHeaders: Authorization: " + headers.get("Authorization"));
   } else {
     console.log("setHeaders: immature server information.");
   }
   return headers;
 }
 
-async function doFetch(target: string, cb: CodebeamerServer) {
+async function doFetch(target: string, cb: cbinit) {
   const headers: Headers = setHeaders(cb);
   return await fetch(target, {headers: headers})
       .then((response: Response) => {
@@ -32,9 +34,24 @@ async function doFetch(target: string, cb: CodebeamerServer) {
       })
 }
 
-export function getProjects(cb: CodebeamerServer) {
+/**
+ * Get a list of Codebeamer projects that the user can access to.
+ * @param cb cbinit interface
+ * @return ProjectReference
+  */
+export function getProjects(cb: cbinit) : Promise<ProjectReference[]> {
   const target = cb.serverUrl + "/projects";
   return doFetch(target, cb);
 }
 
-
+/**
+ * Get a list of items that the tracker specified by parameter holds.
+ * @param cb cbinit interface
+ * @param trackerId tracker ID.
+ * @return TrackerItemReferenceSearchResult
+ */
+export function getTrackerItems(cb: cbinit, trackerId: number): Promise<TrackerItemReferenceSearchResult> {
+  // if (! cb || ! trackerId) return;
+  const target = cb.serverUrl + "/trackers/" + trackerId + "/items";
+  return doFetch(target, cb);
+}
--- main_test.ts
+++ main_test.ts
@@ -1,27 +1,32 @@
 import { assertNotEquals } from "@std/assert";
-import { CodebeamerServer, getProjects } from "./main.ts";
-import {ProjectReference} from "./types/codebeamer.d.ts";
+import {cbinit, getProjects, getTrackerItems} from "./main.ts";
+import {ProjectReference, TrackerItemReferenceSearchResult} from "./types/codebeamer.d.ts";
 
 
 Deno.test(async function getProjectTest() {
-  const cb: CodebeamerServer = {} as CodebeamerServer;
+  const cb: cbinit = {} as cbinit;
   cb.username = Deno.env.get("USERNAME");
   cb.password = Deno.env.get("PASSWORD");
   cb.serverUrl = Deno.env.get("SERVER_URL");
-  console.log("test: from env: username: " + cb.username);
-  console.log("test: from env: password: " + cb.password);
-  console.log("test: from env: server  : " + cb.serverUrl);
-  console.log("test: from env: proxy   : " + Deno.env.get("HTTPS_PROXY"));
+  // console.log("test: from env: username: " + cb.username);
+  //console.log("test: from env: password: " + cb.password);
+  //console.log("test: from env: server  : " + cb.serverUrl);
+  //console.log("test: from env: proxy   : " + Deno.env.get("HTTPS_PROXY"));
 
   const res: Array<ProjectReference> = await getProjects(cb);
   // console.log(JSON.stringify(res, null, 2));
-  console.log("test: result has " + res.length + " projects.");
+  // console.log("\n" + "test: there are " + res.length + " projects." + "\n");
   assertNotEquals(res.length, 0);
+})
 
-  // deno-lint-ignore no-explicit-any
-  res.forEach((project): any => {
-    console.log("id = " + project.id + ", name = " + project.name);
-  })
+Deno.test(async function getTrackerItemTest() {
+  const cb: cbinit = {} as cbinit;
+  cb.username = Deno.env.get("USERNAME");
+  cb.password = Deno.env.get("PASSWORD");
+  cb.serverUrl = Deno.env.get("SERVER_URL");
+  const res: TrackerItemReferenceSearchResult = await getTrackerItems(cb, 6545508);
+  // console.log("\n" + "Items in tracker 6545508 is "  + res.total + "\n")
+  assertNotEquals(res.total, 0);
 })
 
 
