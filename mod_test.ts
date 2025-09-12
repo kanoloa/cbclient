646f8872062ae4522dc31af1c782ade247e491a9
kanoloa <kanoloa@gmail.com>
2025-09-12 14:19:13 +0900

    complete queryItems().  Now the function aggregates chunks into single results.

--- mod_test.ts
+++ mod_test.ts
@@ -1,12 +1,5 @@
 import { assertEquals} from "@std/assert";
-import {
-  getProjects,
-  getTrackerItems,
-  getProjects_success,
-  getTrackerItems_success,
-  queryItems,
-  queryItems_success
-} from "./mod.ts";
+import * as cbclient from "./mod.ts";
 import * as types from "./types/index.ts";
 // import {AbstractFieldValue} from "./types/index.ts";
 
@@ -16,32 +9,32 @@ cb.password = Deno.env.get("PASSWORD");
 cb.serverUrl = Deno.env.get("SERVER_URL");
 
 Deno.test(async function getProjectsTest() {
-  const res: Array<types.ProjectReference> = await getProjects(cb);
-  assertEquals(getProjects_success(res), true);
+  const res: Array<types.ProjectReference> = await cbclient.getProjects(cb);
+  assertEquals(cbclient.getProjects_success(res), true);
 })
 
 
 Deno.test(async function getTrackerItemsTest() {
   // const cb: types.cbinit = {} as types.cbinit;
-  const res: types.TrackerItemReferenceSearchResult = await getTrackerItems(cb, 6545508);
-  assertEquals(getTrackerItems_success(res), true);
+  const res: types.TrackerItemReferenceSearchResult = await cbclient.getTrackerItems(cb, 6545508);
+  assertEquals(cbclient.getTrackerItems_success(res), true);
 })
 
 Deno.test(async function getTrackerItemsTest_2() {
   // const cb: types.cbinit = {} as types.cbinit;
-  const res = await getTrackerItems(cb, 999999);  // not exist
-  assertEquals(getTrackerItems_success(res), false);
+  const res = await cbclient.getTrackerItems(cb, 999999);  // not exist
+  assertEquals(cbclient.getTrackerItems_success(res), false);
 })
 
 
 Deno.test(async function queryItemsTest() {
   const query = "project.id IN (363) AND tracker.id IN (6545508)";
-  const res: types.TrackerItemSearchResult = await queryItems(cb, query, 1,500);
+  const res: types.TrackerItemSearchResult = await cbclient.queryItems(cb, query, 1,500);
   // console.log(JSON.stringify(res, null, 2));
   if (res.items != null) {
     console.log("queryItemTest: got " + res.items.length + " items.");
   }
-   assertEquals(queryItems_success(res), true);
+   assertEquals(cbclient.queryItems_success(res), true);
 })
 
 
