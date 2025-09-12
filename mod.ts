6962abc43b84912c5c6bc34e407ce1ba7240e9f8
kanoloa <kanoloa@gmail.com>
2025-09-12 14:08:56 +0900

    filename changes: - main.ts -> mod.ts - main_test.ts -> mod_test.ts

--- mod.ts
+++ mod.ts
@@ -73,15 +73,80 @@ export function getTrackerItems_success(obj: unknown) : obj is types.TrackerItem
 
 /**
  * Query items using cBQL.
- * @param cb
- * @param query
+ * query for items usually divided into small chunks (500 items at maximum).
+ * This function aggregates chunks into single response.
+ * @param cb type cbinit
+ * @param query query string
  * @param page start page
  * @param pageSize how many items should be included in the response.
  */
-
 export async function queryItems(cb: types.cbinit, query: string, page: number = 1, pageSize: number = 100) {
-    const target = cb.serverUrl + "/items/query?page=" + page + "&pageSize=" + pageSize + "&queryString=" + encodeURI(query);
-    return await doFetch(target, cb);
+
+    let total = 0;     /* number of items */
+    let current = 0;   /* current position */
+    const res: types.TrackerItemSearchResult = {total: 0, page: 0, pageSize: 0, items: []};
+    let target = cb.serverUrl + "/items/query?page=" + page + "&pageSize=" + pageSize + "&queryString=" + encodeURI(query);
+
+    let cnt = 0;
+
+    /* get all the chunks and aggregate them into single response. */
+    do {
+
+        let chunk;
+
+        /* access to Codebeamer endpoint to get query result */
+        try {
+            chunk = await doFetch(target, cb);
+        } catch (e) {
+            console.error(e);
+        }
+
+        /* returned response does not match with declared type. */
+        if (! queryItems_success(chunk)) {
+            break;
+        }
+
+        /* preserver total number of items. */
+        if (chunk.total != null) {
+            total = chunk.total;
+            /* argument 'page' is out of range of number of items. */
+            if (total == 0) {
+                console.log("total: 0.  No data found, exiting...")
+                break;
+            }
+        }
+
+        /* when array exists, then process each of them */
+        if (chunk.items != null && Array.isArray(chunk.items)) {
+
+            /* no more data to read. exiting */
+            if (chunk.items.length === 0) {
+                console.log("page reached to the end of data, exiting...")
+                break;
+            }
+
+            /* calculate number of items that have been read. */
+            current = current + chunk.items.length;
+
+            if (cnt === 0 ){
+                res.total = chunk.total;
+                res.page = chunk.page;
+                res.pageSize = chunk.pageSize;
+            }
+            cnt++;
+
+            if (res.items == null) {
+                res.items = [];
+            }
+
+            res.items = res.items.concat(chunk.items);
+        }
+
+        target = cb.serverUrl + "/items/query?page=" + ++page + "&pageSize=" + pageSize + "&queryString=" + encodeURI(query);
+
+    } while (current < total);
+
+     return res;
 }
 
 export function queryItems_success(obj: unknown) : obj is types.TrackerItemSearchResult {
--- mod_test.ts
+++ mod_test.ts
@@ -8,7 +8,7 @@ import {
   queryItems_success
 } from "./mod.ts";
 import * as types from "./types/index.ts";
-import {AbstractFieldValue} from "./types/index.ts";
+// import {AbstractFieldValue} from "./types/index.ts";
 
 const cb: types.cbinit = {} as types.cbinit;
 cb.username = Deno.env.get("USERNAME");
@@ -19,6 +19,7 @@ Deno.test(async function getProjectsTest() {
   const res: Array<types.ProjectReference> = await getProjects(cb);
   assertEquals(getProjects_success(res), true);
 })
+
 
 Deno.test(async function getTrackerItemsTest() {
   // const cb: types.cbinit = {} as types.cbinit;
@@ -32,39 +33,15 @@ Deno.test(async function getTrackerItemsTest_2() {
   assertEquals(getTrackerItems_success(res), false);
 })
 
+
 Deno.test(async function queryItemsTest() {
   const query = "project.id IN (363) AND tracker.id IN (6545508)";
-  const res: types.TrackerItemSearchResult = await queryItems(cb, query, 1,100);
-  // console.log("queried items: " + JSON.stringify(res, null, 2));
-  if (queryItems_success(res)) {
-    if (res.items != null) {
-      console.log("page = " + res.page + ", count = " + res.items.length);
-      let cnt = 1;
-      res.items.forEach((item) => {
-        let str = "line = " + cnt + ", item id = " + item.id;
-        if (item.customFields && item.customFields.length > 0) {
-          const cf: AbstractFieldValue[] = item.customFields;
-          cf.forEach((field: AbstractFieldValue) => {
-            switch (field.name) {
-              case('CCPM Task Level'):
-                str = str + ", level = " + field.value;
-                break;
-              case ('CCPM Task Code'):
-                str = str + ", code = " + field.value;
-                break;
-              case ('CCPM Task Started'):
-                str = str + ", started? = " + field.value;
-                break;
-            }
-          })
-        }
-        str += ', name = ' + item.name;
-        console.log(str);
-        cnt++;
-      })
-    }
+  const res: types.TrackerItemSearchResult = await queryItems(cb, query, 1,500);
+  // console.log(JSON.stringify(res, null, 2));
+  if (res.items != null) {
+    console.log("queryItemTest: got " + res.items.length + " items.");
   }
-  assertEquals(queryItems_success(res), true);
+   assertEquals(queryItems_success(res), true);
 })
 
 
